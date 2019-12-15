3376f038ca5ec7902a715fe5c2b5353c5cea12e0
FlyingWolFox <49326973+FlyingWolFox@users.noreply.github.com>
2019-12-14 22:10:54 -0300

    Dynamic allocation implemented!! Now there's no size limit

--- array-to-ascii.c
+++ array-to-ascii.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main(int argc, char** argv)
@@ -17,20 +18,16 @@ int main(int argc, char** argv)
 	}
 
 	// opening file
-	FILE* textToConvert;
-	textToConvert = fopen(argv[1], "r+");
-	if (textToConvert == NULL)
+	FILE* textFile;
+	textFile = fopen(argv[1], "r+");
+	if (textFile == NULL)
 	{
 		puts("Failed to open file");
 		return 2;
 	}
 
-	// used to store the textFile
-	// To Do: use malloc()
-	char textFile[100][155];
-
 	// set the program mode
-
+	char* prompt = malloc(3 * sizeof(char));
 	if (argc == 3 && (argv[2][0] == '1' || argv[2][0] == '0'))
 	{
 		mode = argv[2][0] - '0';
@@ -38,150 +35,165 @@ int main(int argc, char** argv)
 
 	else
 	{
-		puts("Which mode:\n1- arrray initialization. Ex: {'E', 'x'}\n2- individual array assignament. Ex: array_0[0][0] = 'E';");
-		fgets(textFile[0], 5, stdin);
-		if (textFile[0][0] == '2' || textFile[0][0] == '1')
-			mode = textFile[0][0] - '1';
+		puts("Which mode:\n1- array initialization. E.g.: {'E', 'x'}\n2- individual array assignament. E.g.: array_0[0][0] = 'E';");
+		fgets(prompt, 3, stdin);
+		if (prompt[0] == '2' || prompt[0] == '1')
+			mode = prompt[0] - '1';
 	}
+	free(prompt);
 
-	// stores the text file in the array
-	int lineCount;
-	for (lineCount = 0; lineCount < 100; lineCount++)
+	// creates the variable to store the text file
+	// and stores it
+	size_t* fileSize = malloc(sizeof(size_t));
+	fseek(textFile, 0L, SEEK_END);
+	*fileSize = ftell(textFile);
+	fseek(textFile, 0L, SEEK_SET);
+
+	char* textToConvert = malloc(*fileSize);
+	//fgets(textToConvert, fileSize + 1, textFile);
+
+	size_t* cursor = malloc(sizeof(size_t));
+
+	*fileSize = 0;
+	do
 	{
-		if (fgets(textFile[lineCount], 155, textToConvert) == NULL)
-			break;
+		textToConvert[*fileSize] = fgetc(textFile);
+		(*fileSize)++;
+	} while (textToConvert[(*fileSize) - 1] != EOF);
 
-		if (textFile[lineCount][154] == '\0')
-			printf("Possible line too big (more than 154 characters). Line %i\n", lineCount);
+	(*fileSize) -= 1;
+	textToConvert[*fileSize] = '\0';
 
-		if (lineCount + 1 == 100)
-			puts("File bigger than supported, last succeful line: 100");
-	}
-
-	// close the file and opens again at the end
-	fclose(textToConvert);
-	textToConvert = fopen(argv[1], "a");
+	fseek(textFile, 0L, SEEK_END);	
 
 	// print 10 new line
 	for (int count = 0; count < 10; count++)
-		fprintf(textToConvert, "\n");
+		fprintf(textFile, "\n");
 	
 	// the program itself
 
 	if (mode == 0)
 	{
-		for (int i = 0; i < lineCount + 1; i++)
+		for (size_t count = 0; count < *fileSize + 1; count++)
 		{
 			// if there's no character in the line, print a enter and goes to the next line
-			if (textFile[i][0] == '\0' || textFile[i][0] == '\n')
+			if (textToConvert[count] == '\0' || textToConvert[count] == '\n')
 			{
-				fprintf(textToConvert, "\n");
+				fprintf(textFile, "\n");
 				continue;
 			}
 
 			// looks if there's a empty line before a non-empty line to put a open bracket
-			if (i > 0)
+			if (count > 0)
 			{
-				if (textFile[i - 1][0] == '\n' && (textFile[i][0] != '\n' && textFile[i][0] != '\0'))
-					fprintf(textToConvert, "{ ");
+				if (textToConvert[count - 1] == '\n' && (textToConvert[count] != '\n' && textToConvert[count] != '\0'))
+					fprintf(textFile, "{ ");
 			}
 
 			// if the file doesn't start with an empy line, put the open bracket
-			if (i == 0 && textFile[i][0] != '\0' && textFile[i][0] != '\n')
-				fprintf(textToConvert, "{ ");
+			if (count == 0 && textToConvert[count] != '\0' && textToConvert[count] != '\n')
+				fprintf(textFile, "{ ");
 
 			// print the line bracket
-			fprintf(textToConvert, "{");
+			fprintf(textFile, "{");
 
 			// the algorithm. It'll get a string and trasnform in a "initializable string"
 			// putting the ' before and after the character, spliting the caracter with commas
 			// something like "Example" to "'E', 'x', 'a', 'm', 'p', 'l', 'e'"
-			for (int j = 0; j < 155; j++)
+			for (; count < *fileSize; count++)
 			{
 				// if the line is empty, go to the next
-				if (textFile[i][j] == '\0' || textFile[i][j] == '\n')
+				if (textToConvert[count] == '\0' || textToConvert[count] == '\n')
 					break;
 
 				// if th echaracter is a backslash or a quote, put a backslash before it
-				if (textFile[i][j] == '\\' || textFile[i][j] == '\"')
-					fprintf(textToConvert, "'\\%c'", textFile[i][j]);
+				if (textToConvert[count] == '\\' || textToConvert[count] == '\"')
+					fprintf(textFile, "'\\%c'", textToConvert[count]);
 
 				// prints the character betwen '
 				else
-					fprintf(textToConvert, "'%c'", textFile[i][j]);
+					fprintf(textFile, "'%c'", textToConvert[count]);
 
 				// if the next line isn't empty, print the comma
-				if (textFile[i][j + 1] != '\0' && textFile[i][j + 1] != '\n')
-					fprintf(textToConvert, ", ");
+				if (textToConvert[count + 1] != '\0' && textToConvert[count + 1] != '\n')
+					fprintf(textFile, ", ");
 			}
 
 			// looks if the lineCount won't be surpassed, so 
 			// the next instructions can be done safely
-			if (i + 1 < lineCount + 1)
+			if (count + 1 < *fileSize + 2)
 			{
 				// if the next line is empty, prints the close bracket of the line bracket 
 				// and the close bracket from the array initializer
-				if (textFile[i + 1][0] == '\0' || textFile[i + 1][0] == '\n')
-					fprintf(textToConvert, "} }\n");
+				if (textToConvert[count + 1] == '\0' || textToConvert[count + 1] == '\n')
+					fprintf(textFile, "} }\n");
 
 				// else, print the close bracket of the line and a comma
 				else
-					fprintf(textToConvert, "},\n");
+					fprintf(textFile, "},\n");
 			}
 
 			// else, we're in the end of the file, just
 			// print the end brackets
 			else
-				fprintf(textToConvert, "} }\n");
+				fprintf(textFile, "} }\n");
 		}
 	}
 
 	if (mode == 1)
 	{
-		int arrayCounter = 0;
-		for (int i = 0; i < lineCount + 1; i++)
+		// using size_t because it can hold the maximum value (32/64 bits)
+		size_t arrayCounter = 0;
+		size_t lineCounter = 0;
+		for (size_t count = 0; count < *fileSize + 1; count++)
 		{
 			// if there's no character in the line, print a enter and goes to the next line
-			if (textFile[i][0] == '\0' || textFile[i][0] == '\n')
+			if (textToConvert[count] == '\0' || textToConvert[count] == '\n')
 			{
-				fprintf(textToConvert, "\n");
+				fprintf(textFile, "\n");
+				lineCounter++;
 				continue;
 			}
 
 			// will update the array index, so the number in array_x updates
-			if (i > 0)
+			if (count > 1)
 			{
-				if (textFile[i - 1][0] == '\n' && textFile[i][0] != '\n' && textFile[i][0] != '\0')
+				if (textToConvert[count - 1] == '\n' && textToConvert[count - 2] == '\n' && textToConvert[count] != '\n' && textToConvert[count] != '\0')
 					arrayCounter++;
 			}
 
 			// the algorithm. It'll get a string and trasnform in a "initializable string"
 			// putting the ' before and after the character, spliting the caracter with commas
 			// something like "E" to array_0[0][0] = 'E';
-			for (int j = 0; j < 155; j++)
+			for (; count < *fileSize + 1; count++)
 			{
 				// if the end of the line was reached, get out of the loop
-				if (textFile[i][j] == '\0' || textFile[i][j] == '\n') 
+				if (textToConvert[count] == '\0' || textToConvert[count] == '\n') 
 					break;
 
 				// prints the satrt of the assignment
-				fprintf(textToConvert, "array_%i[%i][%i] = ", arrayCounter, i, j);
+				fprintf(textFile, "array_%i[%i][%i] = ", arrayCounter, lineCounter, count);
 
 				// if th echaracter is a backslash or a quote, put a backslash before it
-				if (textFile[i][j] == '\\' || textFile[i][j] == '\"')
-					fprintf(textToConvert, "'\\%c'", textFile[i][j]);
+				if (textToConvert[count] == '\\' || textToConvert[count] == '\"')
+					fprintf(textFile, "'\\%c'", textToConvert[count]);
 
 				// prints the character betwen '
 				else
-					fprintf(textToConvert, "'%c'", textFile[i][j]);
+					fprintf(textFile, "'%c'", textToConvert[count]);
 
-					fprintf(textToConvert, ";\n");
+					fprintf(textFile, ";\n");
 			}
+
+			lineCounter++;
 		}
 	}
 
+	free(fileSize);
+	fclose(textFile);
 	puts("Done!");
-	fgets(textFile[99], 5, stdin); // freezes the execution so the console window doens't close
+	fgets(textToConvert, 5, stdin); // freezes the execution so the console window doens't close
+	free(textToConvert);
 
 	return 0;
 }
